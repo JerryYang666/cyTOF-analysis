1a70ddc7de89a5ec2ce916a24b92dc725b0ec296
Ruihuang Yang <rxy216@case.edu>
2023-05-19 12:47:56 +0800

    started on treatment comparison.

---
+++ GroupBox.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2023.
+# -*-coding:utf-8 -*-
+"""
+@file: GroupBox.py
+@author: Jerry(Ruihuang)Yang
+@email: rxy216@case.edu
+@time: 5/19/23 12:46
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
